af02fccb92ce6fc06089aa30e7d8aa166e52fa51
João Martins <j.martins@NORDWARE.IO>
2021-03-27 16:40:52 -0300

    Created project and the basics features in basket api service

---
+++ src/BasketSampleSolution/Basket.API/Model/CustomerBasket.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Basket.API.Model
+{
+    public class CustomerBasket
+    {
+        public string BuyerId { get; }
+        public ICollection<BasketItem> Items { get; } 
+
+        public CustomerBasket()
+        {              
+        }
+
+        [JsonConstructor]
+        public CustomerBasket(string buyerId, ICollection<BasketItem> items)
+        {
+            BuyerId = buyerId;
+            Items = items;
+        }
+    }
+}
---
+++ src/BasketSampleSolution/Basket.API/Startup.cs
@@ -0,0 +1,58 @@
+using Basket.API.Infrastructure.Repositories;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+
+namespace Basket.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+
+            services.AddSingleton(sp =>
+            {                
+                var configuration = ConfigurationOptions.Parse("localhost:6379", true);
+
+                configuration.ResolveDns = true;
+
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+
+            services.AddTransient<IBasketRepository, RedisBasketRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
